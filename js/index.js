a4327deff3e189397b3f4e6a87e132ff23145192
aemartos <aemartos@gmail.com>
2020-12-29 16:44:09 +0100

    change favicon according browser theme

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Change background color (randomly selected)
 const bgColors = [
     '#FFBF00',
     '#FF5252',
@@ -6,3 +7,26 @@ const bgColors = [
     '#894598'
 ];
 document.body.style.background = bgColors[Math.floor(Math.random() * 5)];
+
+// Set favicon according to browser color scheme
+let colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+const setColorScheme = e => {
+    if (e.matches) {
+        // Dark theme
+        var favicon = document.querySelectorAll('link[data-type="favicon"]');
+        var i = favicon.length;
+        while (i--) {
+            favicon[i].setAttribute('href', favicon[i].dataset.light);
+        }
+    } else {
+        // Light theme
+        var favicon = document.querySelectorAll('link[data-type="favicon"]');
+        var i = favicon.length;
+        while (i--) {
+            favicon[i].setAttribute("href", favicon[i].dataset.dark);
+        }
+    }
+}
+
+setColorScheme(colorSchemeQueryList);
+colorSchemeQueryList.addListener(setColorScheme);
